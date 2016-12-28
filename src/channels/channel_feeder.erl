ecba07cb2591fa360861b16a04a28cbbaeafdd17
zack <zack.bitcoin@gmail.com>
2016-12-28 20:20:17 +0200

    check

---
+++ src/channels/channel_feeder.erl
@@ -0,0 +1,26 @@
+-module(channel_feeder).
+-behaviour(gen_server).
+-export([start_link/0,code_change/3,handle_call/3,
+	 handle_cast/2,handle_info/2,init/1,terminate/2,
+	 make/1,grow/1,current/1,spend/1,close/2,
+	 lock_spend/1,bet/1]).
+init(ok) -> {ok, []}.
+start_link() -> gen_server:start_link({local, ?MODULE}, ?MODULE, ok, []).
+code_change(_OldVsn, State, _Extra) -> {ok, State}.
+terminate(_, _) -> io:format("died!"), ok.
+handle_info(_, X) -> {noreply, X}.
+handle_cast(_, X) -> {noreply, X}.
+handle_call(_, _From, X) -> {reply, X, X}.
+
+make(Tx) ->%making a new_channel_tx
+    ok.
+grow(Tx) ->%making a grow_channel_tx
+    ok.
+spend(SPK) ->%giving us money in the channel.
+    ok.
+close(Cid, SS) ->%closing the channel at it's current state
+    ok.
+lock_spend(SPK) ->%giving us money conditionally, and asking us to forward it with a similar condition to someone else.
+    ok.
+bet(SPK) -> %doing one of the bets that we offer.
+    ok.
