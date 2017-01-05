b0cc7ffd6ef7e8eb5759b10e3cb0001b826af3df
Yanislav Malahov <yani@malahov.eu>
2017-01-05 12:47:56 +0200

    minor fix and removed unnecessary file

--- src/consensus/README.md
+++
@@ -1,2 +0,0 @@
-Changing some parts of Flying Fox ends up breaking consensus. When you change the code to break consensus, it is called "hard forks" and "soft forks". When you change the code to break consensus, you are creating a new blockchain.
-This file contains the parts of Flying Fox that you can not change without having a hard or soft fork.
--- src/consensus/mine.erl
+++ src/consensus/mine.erl
@@ -8,17 +8,17 @@ start_link() -> gen_server:start_link({local, ?MODULE}, ?MODULE, ok, []).
 code_change(_OldVsn, State, _Extra) -> {ok, State}.
 terminate(_, _) -> io:format("died!"), ok.
 handle_info(_, X) -> {noreply, X}.
-handle_cast(mine, go) -> 
-    spawn(fun() -> 
+handle_cast(mine, go) ->
+    spawn(fun() ->
 		  block:mine_blocks(10, 50000),
 		  mine() end),
     spawn(fun() -> easy:sync() end),
     {noreply, go};
-handle_cast(start, _) -> 
+handle_cast(start, _) ->
     Cores = erlang:system_info(logical_processors_available),
     io:fwrite("start mining with "),
     io:fwrite(integer_to_list(Cores)),
-    io:fwrite(" many cores.\n"),
+    io:fwrite(" cores.\n"),
     {noreply, go};
 handle_cast(stop, _) ->
     {noreply, stop};
@@ -26,12 +26,12 @@ handle_cast(_, X) -> {noreply, X}.
 handle_call(status, _From, X) -> {reply, X, X};
 handle_call(_, _From, X) -> {reply, X, X}.
 
-start() -> 
+start() ->
     gen_server:cast(?MODULE, start),
     mine().
 mine() ->
     gen_server:cast(?MODULE, mine).
-	   
+
 stop() -> gen_server:cast(?MODULE, stop).
 status() ->
     gen_server:call(?MODULE, status).
