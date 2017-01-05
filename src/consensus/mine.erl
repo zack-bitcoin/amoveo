b4a5eea5b52ec78d9ad4e463522de18b88f619a8
zack <zack.bitcoin@gmail.com>
2017-01-05 12:22:35 +0200

    changed name of function that tells if we are currently mining

--- src/consensus/mine.erl
+++ src/consensus/mine.erl
@@ -2,7 +2,7 @@
 -behaviour(gen_server).
 -export([start_link/0,code_change/3,handle_call/3,handle_cast/2,
 	 handle_info/2,init/1,terminate/2,
-	 start/0,stop/0,status/0]).
+	 start/0,stop/0,is_on/0]).
 init(ok) -> {ok, stop}.
 start_link() -> gen_server:start_link({local, ?MODULE}, ?MODULE, ok, []).
 code_change(_OldVsn, State, _Extra) -> {ok, State}.
@@ -33,5 +33,5 @@ mine() ->
     gen_server:cast(?MODULE, mine).
 	   
 stop() -> gen_server:cast(?MODULE, stop).
-status() ->
+is_on() ->
     gen_server:call(?MODULE, status).
