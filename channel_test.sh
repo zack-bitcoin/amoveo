9bc34db4a15831c3f7f43105097740d22b2ddf64
zack <zack.bitcoin@gmail.com>
2017-01-10 19:39:04 +0200

    typo

--- channel_test.sh
+++ channel_test.sh
@@ -13,6 +13,7 @@ curl -i -d '["sync", [127,0,0,1], 3020]' http://localhost:3011
 sleep 2
 
 curl -i -d '["make_channel", [127,0,0,1], 3020, 10000, 4000, 0, 4, 2]' http://localhost:3011
+#curl -i -d '["sync", [127,0,0,1], 3020]' http://localhost:3011
 #sleep 1
 #curl -i -d '["mine_block"]' http://localhost:3011
 #curl -i -d '["sync", [127,0,0,1], 3020]' http://localhost:3011
--- src/networking/internal_handler.erl
+++ src/networking/internal_handler.erl
@@ -158,7 +158,8 @@ doit({make_channel, IP, Port, MyBal, OtherBal, Rent, Fee, CID}) ->
     Entropy = channel_feeder:entropy(CID, [ID, Acc2]) + 1,
     {Tx, _} = new_channel_tx:make(CID, Accounts, keys:id(), Acc2, MyBal, OtherBal, Rent, Entropy, Fee),
     STx = keys:sign(Tx, Accounts),
-    {ok, _} = talker:talk({new_channel, STx}, IP, Port);
+    talker:talk({new_channel, STx}, IP, Port),
+    {ok, 0};
     
 doit(X) ->
     io:fwrite("don't know how to handle it \n"),
