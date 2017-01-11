6d37892e21f626f7f90a099377c55c30c18984bb
zack <zack.bitcoin@gmail.com>
2017-01-11 16:00:22 +0200

    it is now possible to spend money with channels

--- channel_test.sh
+++ channel_test.sh
@@ -24,6 +24,6 @@ curl -i -d '["mine_block"]' http://localhost:3011
 sleep 1
 curl -i -d '["sync", [127,0,0,1], 3020]' http://localhost:3011
 sleep 1
-echo "Now Channel Spend\n"
 curl -i -d '["channel_spend", [127,0,0,1], 3020, 27]' http://localhost:3011
+sleep 1
 
