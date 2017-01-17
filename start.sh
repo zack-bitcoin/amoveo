d1dd518cbbdb6b48dd8e7e992a1a2fb3500c3532
zack <zack.bitcoin@gmail.com>
2017-01-17 14:10:12 +0200

    automatically clean old blocks

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 # sh start 3666
 
 
-#sh clean.sh #this deletes the database so every time we re-start, we have 0 blocks again. only needed during testing.
+sh clean.sh #this deletes the database so every time we re-start, we have 0 blocks again. only needed during testing.
 ./rebar get-deps
 #sh clean.sh #this deletes the database so every time we re-start, we have 0 blocks again. only needed during testing.
 ./rebar compile #this line checks if any modules were modified, and recompiles them if they were. only needed during testing.
