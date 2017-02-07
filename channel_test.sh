9d63ebca6fb315769a6788a51cda0118a4c3da3d
zack <zack.bitcoin@gmail.com>
2017-02-07 18:39:03 +0200

    typo

--- channel_test.sh
+++ channel_test.sh
@@ -26,12 +26,12 @@ sleep 1
 curl -i -d '["sync", [127,0,0,1], 3020]' http://localhost:3011
 sleep 1
 curl -i -d '["channel_spend", [127,0,0,1], 3020, 27]' http://localhost:3011
-sleep 1
-curl -i -d '["sync", [127,0,0,1], 3020]' http://localhost:3011
-sleep 1
+#sleep 1
+#curl -i -d '["sync", [127,0,0,1], 3020]' http://localhost:3011
+#sleep 1
 
-curl -i -d '["dice", 1000, [127,0,0,1], 3020]' http://localhost:3011 # "dice"
-sleep 1
+#curl -i -d '["dice", 1000, [127,0,0,1], 3020]' http://localhost:3011 # "dice"
+#sleep 1
 #curl -i -d '["mine_block", 1, 1000000]' http://localhost:3021
 #sleep 1
 
