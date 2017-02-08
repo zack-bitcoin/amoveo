3791d9717eb48e1df0cc96aa997e5480b6751305
zack <zack.bitcoin@gmail.com>
2017-02-08 19:27:25 +0200

    typo

--- src/networking/internal_handler.erl
+++ src/networking/internal_handler.erl
@@ -58,8 +58,8 @@ doit({dice, Amount, IP, Port}) ->
     {ok, SSPK, OtherCommit} = talker:talk({dice, 1, MyID, Commit, Amount}, IP, Port),
     SSPK2 = channel_feeder:agree_bet(dice, SSPK, [Amount, Commit, OtherCommit], Secret),%should store partner's info into channel manager.
     SPK = testnet_sign:data(SSPK2),
-    SS1 = dice:make_ss(SPK, Secret);
-    %{ok, SSPKsimple, TheirSecret} = talker:talk({dice, 2, MyID, SSPK2, SS1}, IP, Port), %SSPKsimple doesn't include the bet. the result of the bet instead is recorded.
+    SS1 = dice:make_ss(SPK, Secret),
+    {ok, SSPKsimple, TheirSecret} = talker:talk({dice, 2, MyID, SSPK2, SS1}, IP, Port); %SSPKsimple doesn't include the bet. the result of the bet instead is recorded.
     %SS = dice:resolve_ss(SPK, Secret, TheirSecret),%
     %SSPK2simple = channel_feeder:agree_simplification(dice, SSPKsimple, SS),
     %SPKsimple = testnet_sign:data(SSPKsimple),
