c0ff4c47c448b1816cfa0e0e02a5e31e2bb7f34a
Zwilla <michael.padilla@zwilla.de>
2017-04-11 10:43:08 +0200

    added command for converting to hex

--- src/consensus/testnet_hasher.erl
+++ src/consensus/testnet_hasher.erl
@@ -2,3 +2,10 @@
 -export([doit/1]).
 
 doit(X) -> hash:doit(X, constants:hash_size()).
+bin_to_hex(<<>>) -> "";
+bin_to_hex(<<A, B/binary>>) ->
+  byte_to_hex(<<A>>) ++ bin_to_hex(B).
+
+byte_to_hex(<< N1:4, N2:4 >>) ->
+
+  [erlang:integer_to_list(N1, 16), erlang:integer_to_list(N2, 16)].
