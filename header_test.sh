c073f34d06ac558999bc9cd0402865075345ff20
Zwilla <michael.padilla@zwilla.de>
2017-04-14 03:10:26 +0200

    removing http, so ip4 or ip6 or ssl will be possible

--- header_test.sh
+++ header_test.sh
@@ -1,2 +1,21 @@
 #!/usr/bin/env bash
-curl -i -d '["header", 0]' http://localhost:8040
+##############################################################################
+#
+# @category "AEternity - The Oracle Machine"
+# @package "AETERNITY Testnet"
+# @coauthor Miguel Padilla <miguel.padilla@zwilla.de>
+# @copyright (c) 2017 - Miguel Padilla
+# @link "https://www.the-internet-of-money.de/aeternity"
+#
+# According to our dual licensing model, this program can be used either
+# under the terms of the GNU Affero General Public License, version 3,
+# or under a proprietary license.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+##############################################################################
+#
+curl -i -d '["header", 0]' localhost:8040
