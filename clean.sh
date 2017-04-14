43843e6416b1018fddc9dc939ffc5d774fea55ca
Zwilla <michael.padilla@zwilla.de>
2017-04-14 03:08:17 +0200

    thread safe dir creation

--- clean.sh
+++ clean.sh
@@ -1,4 +1,25 @@
 #!/usr/bin/env bash
+##############################################################################
+#
+# @category "AEternity - The Oracle Machine"
+# @package "AETERNITY Testnet"
+# @author zack.bitcoin@gmail.com
+# @link "http://aeternity.com"
+# @coauthor Miguel Padilla <miguel.padilla@zwilla.de>
+# @copyright (c) 2017 - Miguel Padilla
+# @link "https://www.the-internet-of-money.de/aeternity"
+#
+# According to our dual licensing model, this program can be used either
+# under the terms of the GNU Affero General Public License, version 3,
+# or under a proprietary license.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+##############################################################################
+#
 rm data/*.db
 rm blocks/*.db
 
@@ -7,7 +28,7 @@ then
     touch backup/temp.db
     rm backup/*.db
 else
-    mkdir backup
+    mkdir -p backup
 fi
 
 if [ -e "data/keys_backup" ]
