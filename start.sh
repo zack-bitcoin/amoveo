d32999fe5faada26afecea1c194a12a68667b4f2
zack <zack.bitcoin@gmail.com>
2017-04-28 10:27:28 +0200

    fix start.sh for non-testing mode

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #if you want to use a different port, then start like this:
 # sh start 3666
-#git pull origin experimental
+git pull origin master
 
 #./rebar get-deps
 sh clean.sh #this deletes the database so every time we re-start, we have 0 blocks again. only needed during testing.
@@ -8,5 +8,5 @@ sh clean.sh #this deletes the database so every time we re-start, we have 0 bloc
 sh update.sh
 #echo "GO TO THIS WEBSITE -------> http://localhost:8041/login.html"
 #sleep 1
-erl -pa ebin deps/*/ebin/ -eval "application:ensure_all_started(testnet), serve:pw($1), keys:unlock(\"abc\")"
+erl -pa ebin deps/*/ebin/ -eval "application:ensure_all_started(testnet)"
 #peers:add({46,101,103,165}, 8080)"
