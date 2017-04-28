81fcd21e0559dcaba6b5ac1ffe644c2d55d8d261
zack <zack.bitcoin@gmail.com>
2017-04-28 10:48:51 +0200

    last minute touches to hard fork

--- start.sh
+++ start.sh
@@ -1,6 +1,5 @@
 #if you want to use a different port, then start like this:
 # sh start 3666
-git pull origin master
 
 #./rebar get-deps
 sh clean.sh #this deletes the database so every time we re-start, we have 0 blocks again. only needed during testing.
@@ -8,5 +7,4 @@ sh clean.sh #this deletes the database so every time we re-start, we have 0 bloc
 sh update.sh
 #echo "GO TO THIS WEBSITE -------> http://localhost:8041/login.html"
 #sleep 1
-erl -pa ebin deps/*/ebin/ -eval "application:ensure_all_started(testnet)"
-#peers:add({46,101,103,165}, 8080)"
+erl -pa ebin deps/*/ebin/ -eval "application:ensure_all_started(testnet), serve:pw($1), peers:add({46,101,103,165}, 8040)"
