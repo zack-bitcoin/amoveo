fa43b5740a05214149859bf0a5bbefa05e1b5c3e
zack <zack.bitcoin@gmail.com>
2017-04-28 10:55:02 +0200

    fix typo

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 # sh start 3666
 
 #./rebar get-deps
-sh clean.sh #this deletes the database so every time we re-start, we have 0 blocks again. only needed during testing.
+#sh clean.sh #this deletes the database so every time we re-start, we have 0 blocks again. only needed during testing.
 #./rebar compile #this line checks if any modules were modified, and recompiles them if they were. only needed during testing.
 sh update.sh
 #echo "GO TO THIS WEBSITE -------> http://localhost:8041/login.html"
