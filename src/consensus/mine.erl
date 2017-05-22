b5c9aca963e8cf7aaa1d9419a92de7ccf4f0f776
zack <zack.bitcoin@gmail.com>
2017-05-22 10:00:34 -0700

    fix creating new ids when mining

--- src/consensus/mine.erl
+++ src/consensus/mine.erl
@@ -2,22 +2,24 @@
 -behaviour(gen_server).
 -export([start_link/0,code_change/3,handle_call/3,handle_cast/2,
 	 handle_info/2,init/1,terminate/2,
-	 start/0,stop/0,is_on/0]).
+	 start/0,stop/0,status/0]).
 init(ok) -> {ok, stop}.
 start_link() -> gen_server:start_link({local, ?MODULE}, ?MODULE, ok, []).
 code_change(_OldVsn, State, _Extra) -> {ok, State}.
 terminate(_, _) -> io:format("died!"), ok.
 handle_info(_, X) -> {noreply, X}.
 handle_cast(mine, go) ->
-    case free_constants:test_mode() of
+    T = case free_constants:test_mode() of
 	true ->
 	    block:mine_blocks(1, 5, 1),
-	    timer:sleep(60);
+	    60;
 	false -> 
 	    block:mine_blocks(100, 50000),
-	    timer:sleep(5000)
+	    5000
     end,
-    spawn(fun() -> mine() end),
+    spawn(fun() -> 
+		  timer:sleep(T),
+		  mine() end),
     %spawn(fun() -> easy:sync() end),
     {noreply, go};
 handle_cast(start, stop) ->
@@ -41,5 +43,5 @@ mine() ->
     gen_server:cast(?MODULE, mine).
 
 stop() -> gen_server:cast(?MODULE, stop).
-is_on() ->
+status() ->
     gen_server:call(?MODULE, status).
