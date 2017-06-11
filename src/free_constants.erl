cca58945dea0b85b82757addb7dc0428e2f340e8
zack <zack.bitcoin@gmail.com>
2017-06-11 12:13:09 -0600

    sync

--- src/free_constants.erl
+++ src/free_constants.erl
@@ -2,9 +2,7 @@
 %These constants can be different on every node in the network. You can adjust these variables to suit your own situation.
 -compile(export_all).
 
-test_mode() ->
-    false.
-    %true.
+test_mode() -> false.
 trie_size() ->
     10000. %we can adjust this many accounts and channels in all the blocks in free_constants:revert_depth()
 revert_depth() ->
