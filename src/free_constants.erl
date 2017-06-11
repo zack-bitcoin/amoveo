146a80bcad6a0a492feffd9b3657ecc930e4623a
zack <zack.bitcoin@gmail.com>
2017-06-11 12:06:28 -0600

    prepare to update live testnet

--- src/free_constants.erl
+++ src/free_constants.erl
@@ -2,8 +2,8 @@
 %These constants can be different on every node in the network. You can adjust these variables to suit your own situation.
 -compile(export_all).
 
-test_mode() -> %false.
-    true.
+test_mode() -> false.
+    %true.
 
 trie_size() ->
     10000. %we can adjust this many accounts and channels in all the blocks in free_constants:revert_depth()
--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 # sh start 3666
 
 #./rebar get-deps
-sh clean.sh #this deletes the database so every time we re-start, we have 0 blocks again. only needed during testing.
+#sh clean.sh #this deletes the database so every time we re-start, we have 0 blocks again. only needed during testing.
 
 
 sh update.sh
@@ -10,5 +10,5 @@ sh update.sh
 
 #echo "GO TO THIS WEBSITE -------> http://localhost:8041/login.html"
 #sleep 1
-erl -pa ebin deps/*/ebin/ -eval "application:ensure_all_started(testnet), serve:pw($1), keys:unlock(\"abc\")"
-#peers:add({46,101,103,165}, 8080)"
+erl -pa ebin deps/*/ebin/ -eval "application:ensure_all_started(testnet), serve:pw($1), peers:add({46,101,103,165}, 8080)"
+#keys:unlock(\"abc\")"
