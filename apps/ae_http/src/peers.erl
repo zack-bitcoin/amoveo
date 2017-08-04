8a1e05d00d43c8fe13c895b21c91493ba57457a5
Artur Puzio <cytadela8@interia.pl>
2017-08-04 20:06:57 +0200

    Tabs->Spaces, State record, better variable names

--- apps/ae_http/src/peers.erl
+++ apps/ae_http/src/peers.erl
@@ -10,42 +10,44 @@
          remove/1, %Remove a Peer
          all/0, %Get list of all Peers
          read/1, %Get properties of Peer
-	 update/2 %Update properties of Peer
+         update/2 %Update properties of Peer
 ]).
 
 -record(r, {}). %that's empty for now, in future we will implement ranking mechanics and store bunch of properties here
+-record(s, {peers}). %state record
+
 init(ok) ->
     erlang:send_after(1000, self(), set_initial_peers),
-    {ok, dict:new()}.
+    {ok, #s{peers = dict:new()}}.
 start_link() -> gen_server:start_link({local, ?MODULE}, ?MODULE, ok, []).
-code_change(_OldVsn, State, _Extra) -> {ok, State}.
+code_change(_OldVsn, S, _Extra) -> {ok, S}.
 terminate(_, _) -> io:format("died!"), ok.
 
-handle_info(set_initial_peers, State) ->
+handle_info(set_initial_peers, S) ->
     {ok, Peers} = application:get_env(ae_core, peers),
     add(Peers),
-    {noreply, State};
-handle_info(_Info, State) ->
-    {noreply, State}.
+    {noreply, S};
+handle_info(_Info, S) ->
+    {noreply, S}.
 
-handle_cast({remove, {_,_}=K}, X) ->
-    NewX = dict:erase(K, X),
-    {noreply, NewX};
-handle_cast({add, {_,_}=K}, X) -> 
-    NewX = load_peers([K], X),
-    {noreply, NewX};
-handle_cast({update, {_,_}=K, Properties}, X) ->
-    NewX = dict:store(K, Properties, X),
-    {noreply, NewX}.
+handle_cast({remove, {_,_}=Peer}, S) ->
+    NewPeers = dict:erase(Peer, S#s.peers),
+    {noreply, S#s{peers = NewPeers}};
+handle_cast({add, {_,_}=Peer}, S) -> 
+    NewPeers = load_peers([Peer], S#s.peers),
+    {noreply, S#s{peers = NewPeers}};
+handle_cast({update, {_,_}=Peer, NewProperties}, S) ->
+    NewPeers = dict:store(Peer, NewProperties, S#s.peers),
+    {noreply, S#s{peers = NewPeers}}.
 
-handle_call(all, _From, X) ->
-    {reply, dict:fetch_keys(X), X};
-handle_call({read, {_,_}=K}, _From, X) ->
-    O = case dict:find(K, X) of
+handle_call(all, _From, S) ->
+    {reply, dict:fetch_keys(S#s.peers), S};
+handle_call({read, {_,_}=Peer}, _From, S) ->
+    Properties = case dict:find(Peer, S) of
         error -> <<"none">>;
         {ok, Val} -> Val
     end,
-    {reply, O, X}.
+    {reply, Properties, S}.
 
 all() -> gen_server:call(?MODULE, all).
 
@@ -60,9 +62,9 @@ add([MalformedPeer|T]) ->
     add(T);
 add({IP, Port}) -> 
     NIP = if
-	      is_tuple(IP) -> IP;
-	      is_list(IP) -> list_to_tuple(IP)
-	  end,
+              is_tuple(IP) -> IP;
+              is_list(IP) -> list_to_tuple(IP)
+          end,
     gen_server:cast(?MODULE, {add, {NIP, Port}}).
 
 update(Peer, Properties) ->
@@ -72,11 +74,11 @@ remove(Peer) -> gen_server:cast(?MODULE, {remove, Peer}).
 
 read(Peer) -> gen_server:call(?MODULE, {read, Peer}).
 
-load_peers([], D) -> D;
-load_peers([{_,_}=K|T], Dict) ->
-    D2 = case dict:find(K, Dict) of
-	     error ->
-		 dict:store(K, #r{}, Dict);
-	     _ -> Dict
-	 end,
-    load_peers(T, D2).
+load_peers([], Dict) -> Dict;
+load_peers([{_,_}=Peer|T], Dict) ->
+    NewDict = case dict:find(Peer, Dict) of
+             error ->
+                 dict:store(Peer, #r{}, Dict);
+             _ -> Dict
+         end,
+    load_peers(T, NewDict).
