8a8e534a26d2be8b5d1c5d836a7fda66c7d09b93
zack <zack.bitcoin@gmail.com>
2017-12-16 05:09:52 -0800

    remove comments about shares that are no longer useful

--- apps/ae_core/src/consensus/txs/channel_team_close_tx.erl
+++ apps/ae_core/src/consensus/txs/channel_team_close_tx.erl
@@ -2,11 +2,9 @@
 
 -module(channel_team_close_tx).
 -export([go/3, make/4, acc1/1, acc2/1, fee/1, amount/1,
-	 %sum_share_amounts/1, 
          aid1/1, aid2/1, id/1]).
 -record(ctc, {aid1 = 0, aid2 = 0, fee = 0,
-	      nonce = 0, id = 0, amount = 0}).%, 
-	      %shares}).
+	      nonce = 0, id = 0, amount = 0}).
 aid1(X) -> X#ctc.aid1.
 aid2(X) -> X#ctc.aid2.
 id(X) -> X#ctc.id.
@@ -15,7 +13,6 @@ fee(Tx) -> Tx#ctc.fee.
 acc1(Tx) -> Tx#ctc.aid1.
 acc2(Tx) -> Tx#ctc.aid2.
 make(ID,Trees,Amount,Fee) ->
-%make(ID,Trees,Amount,Shares,Fee) ->
     Accounts = trees:accounts(Trees),
     Channels = trees:channels(Trees),
     {_, C, CProof} = channels:get(ID, Channels),
@@ -26,35 +23,23 @@ make(ID,Trees,Amount,Fee) ->
     Nonce = accounts:nonce(Acc1),
     Tx = #ctc{id = ID, aid1 = A1, aid2 = A2, 
 	     fee = Fee, nonce = Nonce+1, 
-	     %amount = Amount, shares = Shares},
 	     amount = Amount},
     {Tx, [CProof, Proof1, Proof2]}.
     
-%sum_share_amounts([]) -> 0;
-%sum_share_amounts([H|T]) -> 
-%    shares:amount(H)+
-%	sum_share_amounts(T).
 go(Tx, Dict, NewHeight) ->
     ID = Tx#ctc.id,
     OldChannel = channels:dict_get(ID, Dict),
     false = channels:closed(OldChannel),
     Aid1 = channels:acc1(OldChannel),
     Aid2 = channels:acc2(OldChannel),
-    %ID = channels:id(OldChannel),
     Aid1 = Tx#ctc.aid1,
     Aid2 = Tx#ctc.aid2,
     false = Aid1 == Aid2,
     Dict2 = channels:dict_delete(ID, Dict),
     Bal1 = channels:bal1(OldChannel),
     Bal2 = channels:bal2(OldChannel),
-    %ShareAmount = sum_share_amounts(Tx#ctc.shares) div 2,
     Amount = Tx#ctc.amount,
-    %true = Bal1 + Bal2 >= ShareAmount * 2,
-    %true = Bal1 + Amount - ShareAmount > 0,
-    %true = Bal2 - Amount - ShareAmount > 0,
     Acc1 = accounts:dict_update(Aid1, Dict, Bal1 + Amount, Tx#ctc.nonce, NewHeight),
-    %Acc1 = accounts:dict_update(Aid1, Dict, Bal1 + Amount - ShareAmount, Tx#ctc.nonce, NewHeight),
     Acc2 = accounts:dict_update(Aid2, Dict, Bal2 - Amount, none, NewHeight),
-    %Acc2 = accounts:dict_update(Aid2, Dict, Bal2 - Amount - ShareAmount, none, NewHeight),
     Dict3 = accounts:dict_write(Acc1, Dict2),
     accounts:dict_write(Acc2, Dict3).
