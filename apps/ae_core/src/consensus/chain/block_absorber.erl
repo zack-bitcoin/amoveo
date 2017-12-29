612133d747b1e49a67605747e3b65d1853a86987
zack <zack.bitcoin@gmail.com>
2017-12-28 16:51:41 -0800

    simplify block_absorber.erl

--- apps/ae_core/src/consensus/chain/block_absorber.erl
+++ apps/ae_core/src/consensus/chain/block_absorber.erl
@@ -2,7 +2,6 @@
 -behaviour(gen_server).
 -include("../../spk.hrl").
 
-%% API
 -export([enqueue/1, %% async request
 	 save/1,    %% returns after saving
 	 do_save/1 %% run without gen_server
@@ -21,34 +20,22 @@ handle_cast({doit, BP}, X) ->
 handle_call({doit, BP}, _From, X) -> 
     absorb_internal(BP),
     {reply, ok, X}.
-%garbage() -> gen_server:cast(?MODULE, garbage).
-
-%% API functions
-
 enqueue(InputBlocks) when is_list(InputBlocks) ->
     [enqueue(InputBlock) || InputBlock <- InputBlocks];
 enqueue(InputBlock) ->
-    headers:absorb([block:block_to_header(InputBlock)]),
     gen_server:cast(?MODULE, {doit, InputBlock}).
-
 save(InputBlocks) when is_list(InputBlocks) ->
     [save(InputBlock) || InputBlock <- InputBlocks];
 save(InputBlock) ->
     gen_server:call(?MODULE, {doit, InputBlock}).
-
 absorb_internal(Block) ->
-    %io:fwrite("about to absorb a block \n"),
-    %io:fwrite(packer:pack(Block)),
-    %io:fwrite("\n"),
     BH = block:hash(Block),
     NextBlock = Block#block.prev_hash,
     Height = Block#block.height,
     BHC = block_hashes:check(BH),
     if
         Height == 0 -> ok;
-        BHC ->
-            %io:fwrite("we have seen this block before. so block absorber will ignore it.\n"),
-	    ok;
+        BHC -> ok; %we already have this block.
 	true ->
 	    true = block_hashes:check(NextBlock), %check that the previous block is known.
 	    false = empty == block:get_by_hash(NextBlock), %check that previous block was valid
@@ -77,11 +64,6 @@ absorb_internal(Block) ->
     end.
 do_save(BlockPlus) ->
     CompressedBlockPlus = zlib:compress(term_to_binary(BlockPlus)),
-    binary_to_term(zlib:uncompress(CompressedBlockPlus)), % Sanity check, not important for long-term
     Hash = block:hash(BlockPlus),
     BlockFile = ae_utils:binary_to_file_path(blocks, Hash),
     ok = db:save(BlockFile, CompressedBlockPlus).
-
-
-
-
