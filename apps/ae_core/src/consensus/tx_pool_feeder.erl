e6b56d1a9f56dec5754a5a50424c4ab30822268c
zack <zack.bitcoin@gmail.com>
2018-01-06 07:50:13 -0800

    simplify tx_pool_feeder.erl

--- apps/ae_core/src/consensus/tx_pool_feeder.erl
+++ apps/ae_core/src/consensus/tx_pool_feeder.erl
@@ -1,108 +1,24 @@
 -module(tx_pool_feeder).
 -behaviour(gen_server).
+-export([start_link/0,init/1,handle_call/3,handle_cast/2,handle_info/2,terminate/2,code_change/3]).
+-export([absorb/1, absorb_unsafe/1]).
 
-%% API
--export([absorb/1,
-         absorb_unsafe/1]).
-
--export([start_link/0]).
--export([init/1,
-         handle_call/3,
-         handle_cast/2,
-         handle_info/2,
-         terminate/2,
-         code_change/3]).
-
-%% API functions
-
-absorb([]) ->
-    %Absorb needs to feed every tx into the gen_server seperately.
-    %This way if one tx makes the gen_server die, it doesn't ignore the rest of the txs.
-    ok;
-absorb([H | T]) ->
-    absorb(H),
-    absorb(T);
-absorb(SignedTx) ->
-    gen_server:call(?MODULE, {absorb, SignedTx}).
-
-absorb_unsafe(SignedTx) ->
-    F = tx_pool:data_new(),
-    Trees = tx_pool:trees(F),
-    Height = tx_pool:height(F),
-    Dict = tx_pool:dict(F),
-    absorb_unsafe(SignedTx, Trees, Height, Dict).
-
-absorb_unsafe(SignedTx, Trees, Height, Dict) ->
-    %The trees shows the state after the recent txs
-    %new_trees shows the state after the recent block.
-    %Dict holds the state after applying all the recent txs.
-    Querys = proofs:txs_to_querys([SignedTx], Trees),
-    Facts = proofs:prove(Querys, Trees),
-    Dict2 = proofs:facts_to_dict(Facts, Dict),
-    NewDict = txs:digest_from_dict([SignedTx], Dict2, Height + 1),
-    NewTrees = block:dict_update_trie(Trees, NewDict), 
-    tx_pool:absorb_tx(NewTrees, NewDict, SignedTx).
-verify_proofs([], _) -> true;
-verify_proofs([F|T], Trees) ->
-    Type = proofs:tree(F),
-    true = is_atom(Type),
-    Root = proofs:root(F),
-    Root = Type:root_hash(trees:Type(Trees)),
-    X = Type:verify_proof(
-          Root,
-          proofs:key(F),
-          proofs:value(F),
-          proofs:path(F)),
-    if
-        X -> verify_proofs(T, Trees);
-        true -> false
-    end.
-            
-    
-start_link() ->
-    gen_server:start_link({local, ?MODULE}, ?MODULE, ok, []).
-
-
-%% gen_server callbacks
-
-init(ok) ->
-    {ok, []}.
-
+start_link() -> gen_server:start_link({local, ?MODULE}, ?MODULE, ok, []).
+init(ok) -> {ok, []}.
 handle_call({absorb, SignedTx}, _From, State) ->
     absorb_internal(SignedTx),
     {reply, ok, State};
-handle_call(_, _From, State) ->
-    {reply, State, State}.
+handle_call(_, _, S) -> {reply, S, S}.
+handle_cast(_Msg, S) -> {noreply, S}.
+handle_info(_, S) -> {noreply, S}.
+terminate(_, _) -> io:fwrite("tx_pool_feeder died\n").
+code_change(_, S, _) -> {ok, S}.
 
-handle_cast(_Msg, State) ->
-    {noreply, State}.
-
-handle_info(_Info, State) ->
-    {noreply, State}.
-
-terminate(_Reason, _State) ->
-    io:fwrite("tx_pool_feeder died\n").
-
-code_change(_OldVsn, State, _Extra) ->
-    {ok, State}.
-
-
-%% Internals
-
-is_in(_, []) ->
-    false;
-is_in(STx, [STx2 | T]) ->
-    Tx = testnet_sign:data(STx),
+is_in(_, []) -> false;
+is_in(Tx, [STx2 | T]) ->
     Tx2 = testnet_sign:data(STx2),
-    case Tx == Tx2 of
-        true ->
-            true;
-        false ->
-            is_in(STx, T)
-    end.
-
+    (Tx == Tx2) orelse (is_in(Tx, T)).
 absorb_internal(SignedTx) ->
-    %io:fwrite("absorb internal 0\n"),
     F = tx_pool:data_new(),
     Trees = tx_pool:trees(F),
     Height = tx_pool:height(F),
@@ -114,14 +30,30 @@ absorb_internal(SignedTx) ->
     Type = element(1, Tx),
     Cost = governance:get_value(Type, Governance),
     {ok, MinimumTxFee} = application:get_env(ae_core, minimum_tx_fee),
-    io:fwrite(packer:pack({tx_pool_feeder, Fee, MinimumTxFee + Cost})),
-    io:fwrite("\n"),
+    %io:fwrite(packer:pack({tx_pool_feeder, Fee, MinimumTxFee + Cost})),
+    %io:fwrite("\n"),
     true = Fee > (MinimumTxFee + Cost),
     true = testnet_sign:verify(SignedTx),
-    Out = case is_in(SignedTx, Txs) of
-        true ->
-                  ok;
-        false ->
-                  absorb_unsafe(SignedTx, Trees, Height, Dict)
-    end,
-    Out. 
+    case is_in(testnet_sign:data(SignedTx), Txs) of
+        true -> ok;
+        false -> absorb_unsafe(SignedTx, Trees, Height, Dict)
+    end.
+absorb_unsafe(SignedTx, Trees, Height, Dict) ->
+    Querys = proofs:txs_to_querys([SignedTx], Trees),
+    Facts = proofs:prove(Querys, Trees),
+    Dict2 = proofs:facts_to_dict(Facts, Dict),
+    NewDict = txs:digest_from_dict([SignedTx], Dict2, Height + 1),
+    NewTrees = block:dict_update_trie(Trees, NewDict), 
+    tx_pool:absorb_tx(NewTrees, NewDict, SignedTx).
+absorb([]) -> ok;%if one tx makes the gen_server die, it doesn't ignore the rest of the txs.
+absorb([H|T]) ->
+    absorb(H),
+    absorb(T);
+absorb(SignedTx) ->
+    gen_server:call(?MODULE, {absorb, SignedTx}).
+absorb_unsafe(SignedTx) ->
+    F = tx_pool:data_new(),
+    Trees = tx_pool:trees(F),
+    Height = tx_pool:height(F),
+    Dict = tx_pool:dict(F),
+    absorb_unsafe(SignedTx, Trees, Height, Dict).
