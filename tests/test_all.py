dc6643eecf4258e3b90c6db3668c8184e236473a
zack <zack.bitcoin@gmail.com>
2018-01-13 12:22:49 -0300

    add another integration test that had been accidentally shut off

--- tests/test_all.py
+++ tests/test_all.py
@@ -3,16 +3,26 @@ from header import *
 from lightning import *
 from market import *
 from share_blocks import *
+from spend import *
 
-test_mine_and_sync()
-test_three()
-
+print("header tests")
 test_single()
 test_many()
 
+print("fork tests")
+test_mine_and_sync()
+test_three()
+
+print("spend tx tests")
+spend_test()
+
+print("lightning payment tests")
 lightning_test()
 
+print("channel market tests")
 market_test()
 
+print("share blocks tests")
 share_blocks_test1()
 share_blocks_test2()
+
