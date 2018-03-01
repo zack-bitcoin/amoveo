45c0269b8ced9e80979bca4e999ee6458a5f4538
zack <zack.bitcoin@gmail.com>
2018-03-01 09:22:30 -0300

    change version number for hard fork.

--- apps/amoveo_core/src/consensus/version.erl
+++ apps/amoveo_core/src/consensus/version.erl
@@ -2,4 +2,4 @@
 -export([doit/1]).
 %This is to make transaction replay protection easy. When we fork, we don't want the same txs to be valid on both sides. 
 
-doit(_Height) -> 12.
+doit(_Height) -> 1.
