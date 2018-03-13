e45ea19ff86962a3db59ca125e098359f461c11c
zack <zack.bitcoin@gmail.com>
2018-03-12 21:26:14 -0300

    partial_hash_fix fix

--- apps/amoveo_core/src/consensus/forks.erl
+++ apps/amoveo_core/src/consensus/forks.erl
@@ -5,4 +5,10 @@ get(1) ->
     case application:get_env(amoveo_core, kind) of
 	{ok, "production"} -> 4200;
 	_ -> 0
+    end;
+get(2) ->
+    case application:get_env(amoveo_core, kind) of
+	{ok, "production"} -> 9000;
+	_ -> 3
     end.
+    
