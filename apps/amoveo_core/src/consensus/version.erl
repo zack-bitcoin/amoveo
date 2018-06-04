5c88b4e9f3f19a4b2a88826ed94f7f73fc8f1d7f
zack <zack.bitcoin@gmail.com>
2018-06-03 23:20:59 -0400

    change how version is calculated in test mode

--- apps/amoveo_core/src/consensus/version.erl
+++ apps/amoveo_core/src/consensus/version.erl
@@ -2,5 +2,9 @@
 -export([doit/1]).
 %This is to make transaction replay protection easy. When we fork, we don't want the same txs to be valid on both sides. 
 
-doit(H) when H > 26900 -> 3;
-doit(_Height) -> 2.
+doit(H) ->
+    F4 = forks:get(4),
+    if
+	H > F4 -> 3;
+	true -> 2
+    end.
