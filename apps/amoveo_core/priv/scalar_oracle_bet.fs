a8f3390454d92ca9bf95be38fa6c79ee84715d4b
zack <zack.bitcoin@gmail.com>
2018-08-20 21:43:03 -0400

    first draft of oracle bet done.

--- apps/amoveo_core/priv/scalar_oracle_bet.fs
+++ apps/amoveo_core/priv/scalar_oracle_bet.fs
@@ -43,6 +43,23 @@ macro shsc ( PS L -- L2 )
     then
   then
 ;
+: twotozerofun ( Acc L -- Acc2 )
+  nil ==
+  if
+    drop drop reverse
+  else drop
+    car swap int 2 ==
+    if
+      drop drop int 0
+    else
+      drop drop int 1
+    then
+    rot cons swap recurse call
+  then
+;    
+macro twotozero ( L -- L2 )
+      nil swap twotozerofun call
+;
 
 : binary_convert2 ( L N -- N2 )
   swap nil == if
@@ -56,7 +73,7 @@ macro binary_convert ( L -- N )
 ;
 
 macro bet ( ProofStructure p2 p3 p4 p5 p6 p7 p8 p9 p10 -- delay nonce amount)
-      %unpack the 8 things into a list. Use helper on each.
+      %unpack the 10 things into a list. Use helper on each.
       helper nil cons
       shsc shsc shsc shsc shsc
       shsc shsc shsc shsc
@@ -64,15 +81,15 @@ macro bet ( ProofStructure p2 p3 p4 p5 p6 p7 p8 p9 p10 -- delay nonce amount)
       bad_oracle call
       if %some oracle had a 3
         int 0 int 3 int 10000 MaxPrice @ -
-      else
+      else drop
         dup unresolved_oracle call
 	if %some oracle had a 0
           int 1 int 1 int 10000 MaxPrice @ -
-        else
-          %-for each one convert to a binary bit. 2->0
-          %convert to decimal
-          %convert to amount '(/ (* 10000 B) 255)
-          %return delay nonce amount
+        else drop
+	     twotozero %-for each one convert to a binary bit. 2->0
+	     binary_convert %convert to decimal
+	     int 10000 * int 255 / (Amount)
+	     int 0 swap int 3 swap (delay nonce amount)
         then
       then
 ;
@@ -91,4 +108,5 @@ macro test
       [ int 1 , int 3, int 1, int 2] unresolved_oracle call %0
       [ int 0 , int 3, int 0, int 2] unresolved_oracle call %1
       [ int 0, int 0, int 1, int 1, int 1] binary_convert %7
+      [ int 2, int 2, int 2, int 1] twotozero %[0,0,0,1]
 ;
