f61c5d87b954bc32f7145f0a54eade8d44dedd0a
darknemo <darknemo@ya.ru>
2018-09-26 17:37:56 +0300

    UPD encryption tab

--- apps/amoveo_http/priv/external_web/encryption_interface.js
+++ apps/amoveo_http/priv/external_web/encryption_interface.js
@@ -18,7 +18,7 @@
 	tabs.appendChild(div);
 	nav.appendChild(title);
 
-	var b = button_maker2("encrypt", encrypt);
+	var b = button_maker2("Encrypt", encrypt);
 
 	var msg_to_send = document.createElement("INPUT");
 	msg_to_send.type = "text";
@@ -27,13 +27,13 @@
 	msg_to.type = "text";
 	var encrypted_to_send = document.createElement("div");
 	var to_instructions = document.createElement("label");
-	to_instructions.innerHTML = "to pubkey";
+	to_instructions.innerHTML = "To pubkey";
 
 	var msg_instructions = document.createElement("label");
-	msg_instructions.innerHTML = "message to send";
+	msg_instructions.innerHTML = "Message to send";
 
-	var b2 = button_maker2("decrypt", decrypt);
-	var encrypted_received = document.createElement("INPUT");
+	var b2 = button_maker2("Decrypt", decrypt);
+	var encrypted_received = document.createElement("TEXTAREA");
 	encrypted_received.type = "text";
 	var decrypted_received = document.createElement("div");
 
@@ -45,20 +45,20 @@
 	var fieldset1 = wrapper("fieldset", [to_instructions, msg_to]);
 	var fieldset2 = wrapper("fieldset", [msg_instructions, msg_to_send]);
 
-	append_children(wrap, [b, fieldset1, fieldset2, encrypted_to_send, ]);
-	append_children(wrap, [b2, encrypted_received, decrypted_received]);
+	append_children(wrap, [fieldset1, fieldset2, b, encrypted_to_send, hr()]);
+	append_children(wrap, [encrypted_received, b2, decrypted_received]);
 	append_children(div, [wrap, wrap2]);
 
 	function encrypt() {
 		var t = msg_to_send.value;
 		var to = msg_to.value;
 		var t2 = keys.encrypt(t, to);
-		encrypted_to_send.innerHTML = JSON.stringify(t2);
+		encrypted_to_send.innerHTML = "<pre>"+JSON.stringify(t2)+"</pre>";
 	}
 
 	function decrypt() {
 		var t = encrypted_received.value;
 		var t2 = keys.decrypt(JSON.parse(t));
-		decrypted_received.innerHTML = t2;
+		decrypted_received.innerHTML = "<p class='msg'>"+t2+"</p>";
 	}
 })();
