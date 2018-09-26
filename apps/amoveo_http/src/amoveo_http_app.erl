aad5c14d7505e0411482e88cd60fdfc7c6f3589f
Sergey Elin <se@exante.eu>
2018-09-26 15:39:22 +0300

    Use standard cowboy static file handler for exteranl web (with additional required headers)

--- apps/amoveo_http/src/amoveo_http_app.erl
+++ apps/amoveo_http/src/amoveo_http_app.erl
@@ -6,6 +6,8 @@
 -export([start/2,
          stop/1]).
 
+-export([add_requred_headers_to_external/4]).
+
 %% ===================================================================
 %% Application callbacks
 %% ===================================================================
@@ -33,14 +35,29 @@ start_internal() ->
     ok.
 
 start_external() ->
+    DispatchOpts =
+        case application:get_env(amoveo_core, kind) of
+            {ok, "production"} -> {priv_dir, amoveo_http, "external_web", [{mimetypes, cow_mimetypes, all}]};
+            _ -> {dir, "../../../../apps/amoveo_http/priv", "external_web", [{mimetypes, cow_mimetypes, all}]}
+        end,
     Dispatch =
         cowboy_router:compile(
-          [{'_', [{"/:file", ext_file_handler, []},
-                  {"/", ext_handler, []}
-                 ]}]),
+          [{'_', [{"/", ext_handler, []},
+                  {"/[...]", cowboy_static, DispatchOpts}
+          ]}]),
     {ok, Port} = application:get_env(amoveo_core, port),
     {ok, _} = cowboy:start_http(http, 100,
                                 [{ip, {0, 0, 0, 0}}, {port, Port}],
-                                [{env, [{dispatch, Dispatch}]}]),
+                                [
+                                    {onresponse, fun ?MODULE:add_requred_headers_to_external/4},
+                                    {env, [{dispatch, Dispatch}]}
+                                ]),
     ok.
 
+add_requred_headers_to_external(Code, Headers, _Body, Req) ->
+    Headers2 =
+        case lists:keymember(<<"Access-Control-Allow-Origin">>, 1, Headers) of
+            false -> [{<<"Access-Control-Allow-Origin">>, <<"*">>} | Headers];
+            true -> Headers
+        end,
+    {Code, Headers2, Req}.
