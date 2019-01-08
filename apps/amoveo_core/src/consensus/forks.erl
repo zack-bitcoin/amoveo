3ba1dcafca8a6d14cba34cba07f732d45d3ff97f
zack <zack.bitcoin@gmail.com>
2019-01-08 09:17:44 -0800

    start with all hard updates active

--- apps/amoveo_core/src/consensus/forks.erl
+++ apps/amoveo_core/src/consensus/forks.erl
@@ -8,13 +8,13 @@ common(A, B) ->
     end.
 test_height() -> 0.
     %50.
-get(1) -> common(4200, test_height());
-get(2) -> common(9000, test_height());
-get(3) -> common(9900, test_height());
-get(4) -> common(26900, max(test_height(), constants:retarget_frequency()));
-get(5) -> common(27500, max(test_height(), 1));
-get(6) -> common(27700, test_height());
-get(7) -> common(28135, 40);%test_height()).
-get(8) -> common(36120, test_height());
-get(9) -> common(39500, test_height());
-get(10) -> common(47043, 1).
+get(1) -> common(0, test_height());
+get(2) -> common(0, test_height());
+get(3) -> common(0, test_height());
+get(4) -> common(constants:retarget_frequency(), max(test_height(), constants:retarget_frequency()));
+get(5) -> common(1, max(test_height(), 1));
+get(6) -> common(0, test_height());
+get(7) -> common(40, 40);%test_height()).
+get(8) -> common(0, test_height());
+get(9) -> common(0, test_height());
+get(10) -> common(1, 1).
