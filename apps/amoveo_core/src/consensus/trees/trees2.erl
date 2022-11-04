0083bfd35aefe9cac266371583b7e0dc99e8d4c5
zack <zack.bitcoin@gmail.com>
2022-11-04 20:44:31 -0300

    got a passing test showing the different parts of the verkle tree that we will need to use for amoveo.

--- apps/amoveo_core/src/consensus/trees/trees2.erl
+++ apps/amoveo_core/src/consensus/trees/trees2.erl
@@ -9,6 +9,7 @@ range(N, M) when N < M ->
 test(0) ->
     CFG = tree:cfg(amoveo),
     Loc = 1,
+    Many = 4,
     Pairs = 
         lists:map(
           fun(N) ->
@@ -16,28 +17,62 @@ test(0) ->
                   Val = crypto:strong_rand_bytes(32),
                   Meta = crypto:strong_rand_bytes(8),
                   {Key, Val, Meta}
-          end, range(1, 32)),
+          end, range(1, 4)),
     Leaves = lists:map(
                fun({Key, Val, Meta}) ->
                        leaf_verkle:new(
                          Key, Val, Meta, CFG)
                end, Pairs), 
-    Keys = lists:map(
+    AddKey = <<1:256>>,
+    Keys0 = lists:map(
              fun({Key, _, _}) ->
                      Key
              end, Pairs),
+    Keys = [AddKey| Keys0],
     %io:fwrite(hd(Leaves)),
     %
     %io:fwrite(cfg_verkle:meta(CFG)),
     {Loc2, stem, _} = store_verkle:batch(
                         Leaves, Loc, CFG),
+
+    %normal proof has ~500 bytes overhead. Take ~1 second longer to make that fast proofs.
     {Proof, _} = get_verkle:batch(Keys, Loc2, CFG),
+    %fast proof has ~8000 bytes overhead, but can be made faster.
     {FastProof, _} = 
         get_verkle:batch(Keys, Loc2, CFG, fast),
+
+    %verifying proofs
     Root = stem_verkle:root(
              stem_verkle:get(Loc2, CFG)),
-    {true, Leaves2, _} = 
+    {ProofTree1, _Commit1, _Opening1} = Proof,
+    {ProofTree2, _, _} = FastProof,
+    Root01 = stem_verkle:hash_point(hd(ed:decompress_points([hd(ProofTree1)]))),
+    Root01 = stem_verkle:hash_point(hd(ed:decompress_points([hd(ProofTree2)]))),
+    Root01 = stem_verkle:hash_point(Root),
+    {true, Leaves2, ProofTree3} = 
         verify_verkle:proof(Proof, CFG),
-    {true, Leaves2, _} = 
+    {true, Leaves2, ProofTree3} = 
         verify_verkle:proof(FastProof, CFG),
+
+    %updating a proof
+    AddLeaf = leaf_verkle:new(
+                AddKey, <<27, 0:248>>, <<1:64>>, 
+                CFG),
+    UpdateLeaf = leaf_verkle:new(
+                   element(1, hd(Pairs)),
+                   <<28, 0:248>>, <<2:64>>, CFG),
+    DeleteLeaf = {element(1, hd(tl(Pairs))),
+                  0},
+    ProofTree4 = 
+        verify_verkle:update(
+          ProofTree3, 
+          [AddLeaf, UpdateLeaf, DeleteLeaf], 
+          CFG),
+    %new root of new tree:
+    Root2 = stem_verkle:hash_point(hd(ProofTree4)),
+    
+    %efficiently update the hard drive with the new version. Faster that writing the leaves in a batch, because pedersen commitments are already computed.
+    Loc3 = store_verkle:verified(
+             Loc2, ProofTree4, CFG),
+
     success.
