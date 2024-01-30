8a6eb38168a3730549a6f58f2136e60d70032c04
zack <zack.bitcoin@gmail.com>
2024-01-30 10:56:50 -0300

    restore txs more aggressively.

--- apps/amoveo_core/src/consensus/chain/block_absorber.erl
+++ apps/amoveo_core/src/consensus/chain/block_absorber.erl
@@ -126,6 +126,10 @@ absorb_internal(Block) ->
 			    %tx_pool_feeder:absorb_dump(Block2, tx_reserve:all()),
 			    potential_block:new(),
                             tx_reserve:restore(),
+                            spawn(fun() ->
+                                          timer:sleep(30000),
+                                          tx_reserve:restore()
+                                  end),
 %                            lists:map(fun(Tx) ->
 %                                              tx_pool_feeder:absorb_async([Tx])
 %                                      end, lists:reverse(tx_reserve:all())),
