190c5a42b5d9946c8e890d26d3d9334313a16a2c
zack <zack.bitcoin@gmail.com>
2024-06-30 18:52:37 -0400

    update fork height

--- apps/amoveo_core/src/consensus/trees/trees2.erl
+++ apps/amoveo_core/src/consensus/trees/trees2.erl
@@ -1224,7 +1224,7 @@ verify_proof(Proof0, Things, Height) ->
             true -> Proof0
         end,
     {Proof, []} = 
-        restore_leaves_proof(Proof1, Things, Height),
+        restore_leaves_proof(Proof1, Things, Height),%breaks here...
     CFG = tree:cfg(amoveo),
     {true, Leaves, ProofTree} = 
         verify_verkle:proof(Proof, CFG),
