ef486fa87352575b2a2b3b6bce18f9b7c898a73c
zack <zack.bitcoin@gmail.com>
2025-09-21 05:00:44 -0300

    turn off checkpointing while quick syncing blocks.

--- apps/amoveo_core/src/consensus/sync.erl
+++ apps/amoveo_core/src/consensus/sync.erl
@@ -281,7 +281,11 @@ blocks_process_stream(Data0, MyTopBlock, Peer, TheirBlockHeight) ->
             Data2 = <<Data0/binary, Data/binary>>,
             {Data3, NewTop} = try_process_block(Data2, MyTopBlock),
             NewHeight = NewTop#block.height,
-            checkpoint:make(),
+            case sync_mode:check() of
+                normal ->
+                    checkpoint:make();
+                quick -> ok
+            end,
             DefragPeriod = 300,
             if
                 ((NewHeight rem DefragPeriod) == 0) ->
