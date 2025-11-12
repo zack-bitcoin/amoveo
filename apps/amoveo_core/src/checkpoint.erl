16e83e7cf5e68d9c4fad9d05b108b4fbafa55b2d
zack <zack.bitcoin@gmail.com>
2025-11-12 11:35:38 -0300

    simplify chunking format

--- apps/amoveo_core/src/checkpoint.erl
+++ apps/amoveo_core/src/checkpoint.erl
@@ -1116,16 +1116,16 @@ chunkify(File, Folder) ->
             ok
     end.
 chunkify2(<<>>, _, _) -> ok;
-chunkify2(R0, F, N) when (is_binary(R0) and (size(R0) > 1048576))-> 
-    io:fwrite("chunkify2 " ++ integer_to_list(N) ++ " \n"),
-    <<S:8388608/binary, R/binary>> = R0,
+%chunkify2(R0, F, N) when (is_binary(R0) and (size(R0) > 1048576))-> 
+%    io:fwrite("chunkify2 " ++ integer_to_list(N) ++ " \n"),
+%    <<S:8388608/binary, R/binary>> = R0,
+%    file:write_file(F++chunk_name(N), S),
+%    chunkify2(R, F, N+1);
+chunkify2(<<S:8388608/binary, R/binary>>, F, N) -> 
+    %8388608 is 1 megabyte.
+    io:fwrite("chunkify2" ++ integer_to_list(N) ++ " \n"),
     file:write_file(F++chunk_name(N), S),
     chunkify2(R, F, N+1);
-%chunkify2(<<S:8388608, R/binary>>, F, N) -> 
-    %8388608 is 1 megabyte.
-%    io:fwrite("chunkify2" ++ integer_to_list(N) ++ " \n"),
-%    file:write_file(F++chunk_name(N), <<S:8388608>>),
-%    chunkify2(R, F, N+1);
 chunkify2(R, F, N) when (is_binary(R) and (size(R) < 1100000))-> 
     io:fwrite("chunkify done\n"),
     file:write_file(F ++ chunk_name(N), R),
